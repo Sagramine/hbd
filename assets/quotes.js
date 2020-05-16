672a7d5d74c37587b2707365c19c2d45bc5d7dd7
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-16 15:24:40 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -5,6 +5,4 @@ export default [
     { author: "-ALAN", quotes: "Have a Nice Day zeyengg" },
     { author: "-ALAN", quotes: "I'm sorry, what i've done T_T" },
     { author: "-ALAN", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "-ALAN", quotes: "I miss you." },
-    { author: "-ALAN", quotes: "I'm sorry for loving you." },
 ];
