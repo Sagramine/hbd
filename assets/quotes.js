bc9df185c7e91389e262a5d0ec81ac4cce920dd9
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-16 15:20:53 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "-alan", quotes: "Whatever happens, it's not true :(" },
-    { author: "-alan", quotes: "Hey na! I wuv chuuuu! UwU" },
-    { author: "-alan", quotes: "Have a Nice Day zeyengg" },
-    { author: "-alan", quotes: "I'm sorry, what i've done T_T" },
-    { author: "-alan", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "-alan", quotes: "I miss you." },
-    { author: "-alan", quotes: "I'm sorry for loving you." },
+    { author: "ALAN", quotes: "Whatever happens, it's not true :(" },
+    { author: "ALAN", quotes: "Hey na! I wuv chuuuu! UwU" },
+    { author: "-ALAN", quotes: "Have a Nice Day zeyengg" },
+    { author: "-ALAN", quotes: "I'm sorry, what i've done T_T" },
+    { author: "-ALAN", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "-ALAN", quotes: "I miss you." },
+    { author: "-ALAN", quotes: "I'm sorry for loving you." },
 ];
