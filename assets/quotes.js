3b7630660791e45c0290cf53931bf0a39c14bf0e
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-16 14:41:01 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -3,7 +3,7 @@ export default [
     { author: "-alan", quotes: "Whatever happens, it's not true :(" },
     { author: "-alan", quotes: "Hey na! I wuv chuuuu! UwU" },
     { author: "-alan", quotes: "Have a Nice Day zeyengg" },
-    { author: "-alan", quotes: "I'm sorry, what i've done with you." },
+    { author: "-alan", quotes: "I'm sorry, what i've done T_T" },
     { author: "-alan", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "-alan", quotes: "I miss you." },
     { author: "-alan", quotes: "I'm sorry for loving you." },
