dd535dd5ad257c7f50671983b8ceb3e7485079f7
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-16 15:54:12 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -4,5 +4,4 @@ export default [
     { author: "-ALAN", quotes: "Hey na! I wuv chuuuu! UwU" },
     { author: "-ALAN", quotes: "Have a Nice Day zeyengg" },
     { author: "-ALAN", quotes: "I'm sorry, what i've done T_T" },
-    { author: "-ALAN", quotes: "I may not with you everyday, but I love you everyday." },
 ];
