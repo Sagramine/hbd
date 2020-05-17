fa15ea65d0156d5dddf5faa961457a59b61891d1
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-17 11:29:27 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -3,5 +3,5 @@ export default [
     { author: "-Iqbal", quotes: "Whatever happens, it's not true :(" },
     { author: "-Iqbal", quotes: "Hey na! I wuv chuuuu! UwU" },
     { author: "-Iqbal", quotes: "Have a Nice Day zeyengg" },
-    { author: "-Iqbal", quotes: "I'm sorry, what i've done T_T" },
+    { author: "-Iqbal", quotes: "Pam pam pararam wkwkwkwkwk :v" },
 ];
