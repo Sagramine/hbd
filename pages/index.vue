6616daca81cb1195cf614666dd0c24de05fde519
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-17 11:32:57 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'Aku tahu aku salah, engga harusnya aku bilang gitu ^2500', 'Huhu! (╥﹏╥)']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'I always wanted to tell you... ^2500', 'Hihi! ｡◕‿◕｡']"
         :backDelay="500"
         :typeSpeed="100"
         :backSpeed="20"
