3140f2a5268058c1e71414727e7dcc132988073c
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-05-19 16:46:27 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
-    { author: "-Iqbal", quotes: "Whatever happens, it's not true :(" },
-    { author: "-Iqbal", quotes: "Hey na! I wuv chuuuu! UwU" },
-    { author: "-Iqbal", quotes: "Have a Nice Day zeyengg" },
-    { author: "-Iqbal", quotes: "Whatever happens, keep breathing" },
+    { author: "-Alan", quotes: "Whatever happens, it's not true :(" },
+    { author: "-Alan", quotes: "Hey na! I wuv chuuuu! UwU" },
+    { author: "-Alan", quotes: "Have a Nice Day zeyengg" },
+    { author: "-Alan", quotes: "Whatever happens, keep breathing" },
 ];
