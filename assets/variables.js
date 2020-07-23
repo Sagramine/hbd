38d9c4fa713201ad775118dd3f986085501391d3
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-07-23 20:40:56 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
   nicknames: ["Saki-chan ✨", "Saki-chan >.<"],
   greetings: {
-    evening: "Otanjoubi Omedetou",
-    afternoon: "Otanjoubi Omedetou",
-    day: "Otanjoubi Omedetou",
-    morning: "Otanjoubi Omedetou",
-    night: "Otanjoubi Omedetou"
+    evening: "Otanjoubi omedetou",
+    afternoon: "Otanjoubi omedetou",
+    day: "Otanjoubi omedetou",
+    morning: "Otanjoubi omedetou",
+    night: "Otanjoubi omedetou"
   }
 };
