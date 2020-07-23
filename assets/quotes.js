ebbc70b1c8ea3c639d020a6b9100714ee1903587
iqbalanhari <65439054+IqbalAnhari@users.noreply.github.com>
2020-07-23 19:25:52 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
-    { author: "Alan", quotes: "I may not with you everyday, but I miss you everyday" },
-    { author: "Alan", quotes: "Hey na! I wuv chuuuu! UwU" },
-    { author: "Alan", quotes: "Have a Nice Day zeyengg" },
-    { author: "Alan", quotes: "As long as i live, I miss you" },
+    { author: "Iqbal", quotes: "I may not with you everyday, but I miss you everyday" },
+    { author: "Iqbal", quotes: "Hey na! I wuv chuuuu! UwU" },
+    { author: "Iqbal", quotes: "Have a Nice Day zeyengg" },
+    { author: "Iqbal", quotes: "As long as i live, I miss you" },
 ];
