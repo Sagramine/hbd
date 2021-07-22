0209aff895f265f632b77e1c0284aea123ddec6a
Sagramine <65439054+Sagramine@users.noreply.github.com>
2021-07-22 21:58:41 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,4 +1,4 @@
 // prettier-ignore
 export default [
-    { author: "Iqbal Anhari ", quotes: "Happy 20th Birthday Darling 💝💕💓🎂" },
+    { author: "From Sweetheart, Iqbal ", quotes: "Happy 20th Birthday Darling 💝💕💓🎂" },
 ];
