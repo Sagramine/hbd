0571ab53c283027394cf7df1c8cca95950b817fb
Sagramine <65439054+Sagramine@users.noreply.github.com>
2021-07-23 06:34:22 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'براك الله في عمرك ^4000', 'Wish you all the best ❤️✨']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'براك الله في عمرك ^4000', 'Wish you all the best ❤️💓✨']"
         :backDelay="500"
         :typeSpeed="120"
         :backSpeed="20"
