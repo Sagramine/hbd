64e808f5013948788848fabe24bab7cea8e8834a
Sagramine <65439054+Sagramine@users.noreply.github.com>
2021-07-23 01:15:59 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'براك الله في عمرك ^4000', 'Wish you all the best (˶ ❛ꁞ❛˶)']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'براك الله في عمرك ^4000', 'Wish you all the best 💕✨']"
         :backDelay="500"
         :typeSpeed="130"
         :backSpeed="20"
