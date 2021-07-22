d0ba39236c10331c91233fc6aada2e6c088e8c0f
Sagramine <65439054+Sagramine@users.noreply.github.com>
2021-07-22 23:46:13 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   head: {
     // titleTemplate: "%s - " + process.env.npm_package_name,
     // title: process.env.npm_package_name || "",
-    title: "HBD Mba Sakinah 🎂",
+    title: "Selamat ulang tahun ke-20 💕🎂",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
